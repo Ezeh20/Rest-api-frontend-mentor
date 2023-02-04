0397d6c2767ef19e317f1fb9ab704add0c8189fa
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2023-02-04 22:54:13 +0100

    chore: format country's population number

--- src/Component/Country-Cards/Country-cards.jsx
+++ src/Component/Country-Cards/Country-cards.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import styles from './Country-cards-style.module.scss'
 
 const CountryCards = ({ country }) => {
-
-    console.log(country)
-const { capital, name, region, population, flags } = country
+    const { capital, name, region, population, flags } = country
     const { common } = name
     const { png } = flags
 
-
+    //formatting country population
+    const formatted = new Intl.NumberFormat().format(population)
+    
     return (
         <div className={`${styles.country_card} alt-bg`}>
             <img src={png} alt="flag" className={styles.country_flag} />
             <div className={`${styles.card_content}`}>
-                <span className='alt-text'>{common}</span>
+                <span className={`${styles.country_name} 'alt-text'`}>{common}</span>
                 <div className={styles.country_info}>
-                    <p>Population: {population}</p>
-                    <p>Region: {region}</p>
-                    <p>Capital: {capital}</p>
+                    <p >Population: <span className={styles.country_details}>{formatted}</span></p>
+                    <p >Region: <span className={styles.country_details}>{region}</span></p>
+                    <p >Capital: <span className={styles.country_details}>{capital}</span></p>
                 </div>
             </div>
         </div>

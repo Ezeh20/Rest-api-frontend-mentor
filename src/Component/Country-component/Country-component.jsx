bb8d9173c8387aea1651782f693a1a6b3f143805
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2023-02-05 15:15:59 +0100

    fix: display total countries

--- src/Component/Country-component/Country-component.jsx
+++ src/Component/Country-component/Country-component.jsx
@@ -3,11 +3,11 @@ import { useLocation } from 'react-router-dom'
 import ContainerW from '../../Layout/Container/Container'
 import Layout from '../../Layout/Layout'
 
-
 const CountryComponent = () => {
+    //access the state in the current route which was passed using the useNavigate hook
     const location = useLocation()
-
     const { capital, name, region, population, flags } = location.state
+
     return (
         <div>
             <Layout>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,13 +9,14 @@ import styles from './Home.module.scss'
 
 const Home = () => {
     const { filtered } = useContext(CountriesContext)
-   
+
     return (
         <div>
             <Layout>
                 <ContainerW>
                     <div className={`${styles.home} bg text`}>
                         <SearchComponent />
+                        <p className={styles.total}>({filtered.length})</p>
                         {filtered.length < 1 && <p className={styles.not_found}> No country found</p>}
                         <div className={styles.contain}>
                             {
